809f152dd5cac9b30c31679e0dca2f931f8e2031
ousttrue <ousttrue@gmail.com>
2019-11-20 22:45:42 +0900

    renderer.d

--- source/app.d
+++ source/app.d
@@ -1,9 +1,9 @@
 import core.sys.windows.windows;
-import windowskits;
 import std.stdio;
 import std.conv;
 import std.getopt;
 import core.runtime;
+import renderer;
 
 wstring g_className = "SAMPLE_CLASS_NAME";
 wstring g_windowName = "Window Title";
@@ -21,14 +21,6 @@ extern (Windows) LRESULT WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPar
     }
 
     return DefWindowProc(hwnd, msg, wParam, lParam);
-}
-
-class Renderer
-{
-    void load(wstring path)
-    {
-        writeln(path);
-    }
 }
 
 extern (Windows) int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, // @suppress(dscanner.suspicious.unused_parameter)
@@ -86,46 +78,10 @@ extern (Windows) int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, // @s
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    auto dtype = D3D_DRIVER_TYPE.D3D_DRIVER_TYPE_HARDWARE;
-    UINT flags = 0;
-    auto featureLevels = [
-        D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_11_0,
-        D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_10_1,
-        D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_10_0,
-        D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_9_3,
-        D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_9_2,
-        D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_9_1,
-    ];
-
-    //UINT numFeatureLevels = sizeof(featureLevels) / sizeof(D3D_FEATURE_LEVEL);
-    auto sdkVersion = D3D11_SDK_VERSION;
-    D3D_FEATURE_LEVEL validFeatureLevel;
-
-    DXGI_SWAP_CHAIN_DESC scDesc;
-    scDesc.BufferCount = 1;
-    scDesc.BufferDesc.Width = 0;
-    scDesc.BufferDesc.Height = 0;
-    scDesc.BufferDesc.Format = DXGI_FORMAT.DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
-    scDesc.BufferDesc.RefreshRate.Numerator = 60;
-    scDesc.BufferDesc.RefreshRate.Denominator = 1;
-    scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    scDesc.OutputWindow = hwnd;
-    scDesc.SampleDesc.Count = 1;
-    scDesc.SampleDesc.Quality = 0;
-    scDesc.Windowed = TRUE;
-
-    IDXGISwapChain swapchain;
-    ID3D11Device device;
-    ID3D11DeviceContext context;
-    HRESULT hr = D3D11CreateDeviceAndSwapChain(null, dtype, null, flags, featureLevels.ptr,
-            cast(uint) featureLevels.length, sdkVersion, &scDesc, &swapchain,
-            &device, &validFeatureLevel, &context);
-    if (hr != S_OK)
+    if (!renderer.initialize(hwnd))
     {
         return 3;
     }
-
-    ID3D11RenderTargetView rtv;
 
     MSG Msg;
     while (GetMessage(&Msg, NULL, 0, 0))
@@ -133,32 +89,10 @@ extern (Windows) int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, // @s
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
 
-        if (!rtv)
-        {
-            ID3D11Texture2D backbuffer;
-            hr = swapchain.GetBuffer(0, &backbuffer.iidof, cast(void**)&backbuffer);
-            if (hr != S_OK)
-            {
-                throw new Exception("fail to GetBuffer");
-            }
-            scope (exit)
-                backbuffer.Release();
-
-            hr = device.CreateRenderTargetView(backbuffer, null, &rtv);
-            if (hr != S_OK)
-            {
-                throw new Exception("fail to CreateRenderTargetView");
-            }
-        }
-        float[4] clearColor = [0.0f, 0.0f, 1.0f, 0.0f];
-        context.ClearRenderTargetView(rtv, clearColor);
-
-        // context.Flush();
-
-        swapchain.Present(0, 0);
+        renderer.draw();
     }
 
-    swapchain.Release();
+    destroy(renderer);
 
     return cast(int) Msg.wParam;
 }
---
+++ source/renderer.d
@@ -0,0 +1,94 @@
+module renderer;
+import core.sys.windows.windows;
+import windowskits;
+import std.stdio;
+
+class Renderer
+{
+    IDXGISwapChain m_swapchain;
+    ID3D11Device m_device;
+    ID3D11DeviceContext m_context;
+
+    ID3D11RenderTargetView m_rtv;
+
+    ~this()
+    {
+        if (m_rtv)
+        {
+            m_rtv.Release();
+            m_rtv = null;
+        }
+
+        m_swapchain.Release();
+        m_context.Release();
+        m_device.Release();
+    }
+
+    bool initialize(HWND hwnd)
+    {
+        auto dtype = D3D_DRIVER_TYPE.D3D_DRIVER_TYPE_HARDWARE;
+        UINT flags = 0;
+        auto featureLevels = [
+            D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_11_0,
+            D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_10_1,
+            D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_10_0,
+            D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_9_3,
+            D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_9_2,
+            D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_9_1,
+        ];
+
+        //UINT numFeatureLevels = sizeof(featureLevels) / sizeof(D3D_FEATURE_LEVEL);
+        auto sdkVersion = D3D11_SDK_VERSION;
+        D3D_FEATURE_LEVEL validFeatureLevel;
+
+        DXGI_SWAP_CHAIN_DESC scDesc;
+        scDesc.BufferCount = 1;
+        scDesc.BufferDesc.Width = 0;
+        scDesc.BufferDesc.Height = 0;
+        scDesc.BufferDesc.Format = DXGI_FORMAT.DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+        scDesc.BufferDesc.RefreshRate.Numerator = 60;
+        scDesc.BufferDesc.RefreshRate.Denominator = 1;
+        scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        scDesc.OutputWindow = hwnd;
+        scDesc.SampleDesc.Count = 1;
+        scDesc.SampleDesc.Quality = 0;
+        scDesc.Windowed = TRUE;
+
+        HRESULT hr = D3D11CreateDeviceAndSwapChain(null, dtype, null, flags, featureLevels.ptr,
+                cast(uint) featureLevels.length, sdkVersion, &scDesc,
+                &m_swapchain, &m_device, &validFeatureLevel, &m_context);
+        return hr == S_OK;
+    }
+
+    void load(wstring path)
+    {
+        writeln(path);
+    }
+
+    void draw()
+    {
+        if (!m_rtv)
+        {
+            ID3D11Texture2D backbuffer;
+            auto hr = m_swapchain.GetBuffer(0, &backbuffer.iidof, cast(void**)&backbuffer);
+            if (hr != S_OK)
+            {
+                throw new Exception("fail to GetBuffer");
+            }
+            scope (exit)
+                backbuffer.Release();
+
+            hr = m_device.CreateRenderTargetView(backbuffer, null, &m_rtv);
+            if (hr != S_OK)
+            {
+                throw new Exception("fail to CreateRenderTargetView");
+            }
+        }
+        float[4] clearColor = [0.0f, 0.0f, 1.0f, 0.0f];
+        m_context.ClearRenderTargetView(m_rtv, clearColor);
+
+        // context.Flush();
+
+        m_swapchain.Present(0, 0);
+    }
+}
